66b5bd10aab7e0b32f108db4cbe34fca4c80ae53
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2023-01-30 16:32:03 -0600

    ...

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,39 +1,38 @@
 import java.util.ArrayList;
 
 public class Student {
-    long id;
-    String name;
-    ArrayList<Integer> grades;
 
-    //Student Constructor
-public Student(long id, String name){
-    this.id = id;
-    this.name = name;
-    this.grades = new ArrayList<>();
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
 
-}
-
-    public long getId() {
-        return id;
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public long getId() {
+        return this.id;
     }
 
     public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        return this.name;
     }
 
     public ArrayList<Integer> getGrades() {
-        return grades;
+        return this.grades;
     }
 
-    public void setGrades(ArrayList<Integer> grades) {
-        this.grades = grades;
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        double sum = 0;
+        for (int grade: this.grades) {
+            sum += grade;
+        }
+        return sum / this.grades.size() ;
     }
 }
